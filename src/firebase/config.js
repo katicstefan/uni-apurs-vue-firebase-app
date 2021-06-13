67f0e0f6b9e953242fd22341f076becf1622a766
Stefan Katic <katicstefan.coder@gmail.com>
2021-06-13 17:15:04 +0200

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDInPgzwT21EF2yJCOhdgVWiB_4QS9oL78",
+    authDomain: "uni-apurs.firebaseapp.com",
+    projectId: "uni-apurs",
+    storageBucket: "uni-apurs.appspot.com",
+    messagingSenderId: "353919660330",
+    appId: "1:353919660330:web:724a60ab682369fd74ae53",
+    measurementId: "G-WVCV00X7PV"
+};
+
+const projectFirestore = firebase.initializeApp(firebaseConfig)
+
+export { projectFirestore }
